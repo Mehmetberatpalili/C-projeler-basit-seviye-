6e3b88e16cdb5def4c60dc32e36f6910ba895e3b
Mehmet Berat Palili <96793300+Mehmetberatpalili@users.noreply.github.com>
2022-09-04 19:18:17 +0300

    Add files via upload
    Temel Döngüler

---
+++ for dögüsü 10 kez tekrarlama.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*while(koþul){
+    // çalýþtýrýlacak kodlar
+}
+*/
+int main(){
+     int i=0;
+     while(i<10){
+          printf("mehmetpalili\n");
+          i++;
+      }
+}
+
+
